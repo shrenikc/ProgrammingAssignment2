73dac6617b6bd486bd187e5446c3d90b2c66c260
Shrenik Chougule <c_shrenik@hotmail.com>
2014-07-24 00:28:50 -0700

    Implemented makeCacheMatrix() and cacheSolve() functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R file contains 2 functions makeCacheMatrix(x) and cacheSolve(x). 
+## It improves matrix Inverse functionality by caching the Inverse matrix.
+## It uses Lazy Initialization for matrix Inverse. i.e. Inverse martix is not 
+## cached until it is accessed first
 
-## Write a short comment describing this function
+## Caches matrix and initializes Inverse matrix to NULL
+## Defaults to empty numeric matrix if no argument is passed
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        inverse<-NULL
+        
+        set<- function(y) {
+                x<<- y
+                inverse<<- NULL
+        }
+        
+        get<-function() x
+        
+        setInverse<-function(i){
+                inverse<<-i
+        }
+        getInverse<-function() {
+                inverse
+        }
+        
+        list(set=set, get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function accepts List object returned by makeCacheMatrix(x) and returns 
+## the cached value of Inverse Matrix
+## If Inverse matrix is not cached then it computes Invere Matrix and caches
+## it in List function argument.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse()
+
+        if(!is.null(i)) {
+            message("Returing cached value")
+            i
+        }
+                
+        i <-solve(x$get(),...)
+        x$setInverse(i)
+        i
 }
